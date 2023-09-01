2689d781e9df500f2c0db0fc578ecbc9ca1c674a
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-09-01 16:43:56 +0300

    made it more general

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,6 @@
 $(document).ready(() => $('body').prepend(`
         <div id="loading-overlay"
-            class="bg-black position-absolute top-0 start-0 bottom-0 end-0 z-${location.pathname.endsWith('/') ? 3 : 2} justify-content-center align-items-center text-white" style="display: flex;">
+            class="bg-black position-absolute top-0 start-0 bottom-0 end-0 z-${location.pathname.endsWith('/') || location.pathname.endsWith('index.html') ? 3 : 2} justify-content-center align-items-center text-white" style="display: flex;">
             <i class="fa-solid fa-spinner fa-spin fa-5x"></i>
         </div>
     `));
